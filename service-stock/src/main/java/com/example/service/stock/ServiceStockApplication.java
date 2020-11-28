7e0fa32d77eb8b5fa7452dee5a82a86f776ac080
rechardtang <rechardtang@foxmail.com>
2020-11-28 19:21:57 +0800

    zuul configuration

--- service-stock/src/main/java/com/example/service/stock/ServiceStockApplication.java
+++ service-stock/src/main/java/com/example/service/stock/ServiceStockApplication.java
@@ -1,4 +1,4 @@
-package com.example.service.order;
+package com.example.service.stock;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
